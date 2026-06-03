0434f01e7aa4b55c258f5d0880ed9eaf2400e0b1
Shashank Lakhera <lakherashashank70@gmail.com>
2026-06-04 02:15:25 +0530

    Updated cors config

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
